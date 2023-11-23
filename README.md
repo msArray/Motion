74f9d799803dd54eb421a4ae24c1bf91f4c304ca
K93MSY <masaya93@ogaki-tv.ne.jp>
2023-11-23 17:41:11 +0900

    update conf

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# MotionPath
+# Motion
+## 説明
+PreactとMotion Framerを主に使って作った動くボタンとWindowUIのサンプル
+
+## 開発環境
+- Node.js v18.18.2
+- Npm 9.8.1
+- Vite 5.0.0
+- Preact 10.19.1
+
+## 環境構築
+`
+npm install  
+npm run dev
+`
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import { defineConfig, loadEnv } from "vite";
+import preact from "@preact/preset-vite";
+import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [preact()],
-})
+export default defineConfig(({ command, mode }) => {
+  // `mode` に基づいて現在の作業ディレクトリにある env ファイルをロードする
+  // `VITE_` プレフィックスに関係なく全ての環境変数をロードするには、第 3 引数に '' を設定します
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [preact()],
+    build: {
+      outDir: mode == "development" ? "dist" : "docs",
+    },
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+  };
+});
